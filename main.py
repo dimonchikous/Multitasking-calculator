0078fc37b587c87b1142a4f14d1e7b98e5cde09a
Dimonchikous <dima.maczkovskij3010@gmail.com>
2025-05-28 17:30:04 +1000

    Added time-to-time

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ while menu:
     print("~Menu~")
     print("1| Calculator \n"
         "2| Even or Odd \n"
-        "3| Time to milliseconds \n" 
+        "3| Time-to-time \n" 
         "4| Factorial \n"
         "0| About program")
     ch = input("Do your choose(num): ")
@@ -33,31 +33,124 @@ while menu:
             print("odd")
 
     elif ch == "3" :
-        print("-SubMenu-\n"
-            "!Time to milliseconds!")
-        print("1| Seconds to milliseconds\n"
-            "2| Minutes to milliseconds\n"
-            "3| Hours to milliseconds\n"
-            "4| Days to milliseconds")
-        chmil = input("Do your choose(num): ")
-        time = int(input("Input time(int): "))
-        print(" ")
-        if chmil == "1" :
-            print("Seconds to milliseconds is " + str(time*1000)+ " milliseconds")
-        elif chmil == "2" :
-            print("Minutes to milliseconds is " + str(time*6000)+ " milliseconds")
-        elif chmil == "3" :
-            print("Hours to milliseconds is " + str(time*3600000)+ " milliseconds")
-        elif chmil == "4" :
-            print("Days to milliseconds is " + str(time*86400000) + " milliseconds")
-        else :
-            print("Error")
+        print("/-SubMenu\ ")
+        print("!Time-to-time!")
+        print("1| Time-to-milliseconds\n"
+               "2| Time-to-seconds\n"
+               "3| Time-to-minutes\n"
+                "4| Time-to-hours\n"
+                "5| Time-to-days")
+        chy = input("Do your choose(num): ")
+        if chy == "1" :
+            print("-SubMenu-\n"
+                "!Time-to-milliseconds!")
+            print("1| Seconds-to-milliseconds\n"
+                "2| Minutes-to-milliseconds\n"
+                "3| Hours-to-milliseconds\n"
+                "4| Days-to-milliseconds")
+            chmil = input("Do your choose(num): ")
+            time = int(input("Input time(int): "))
+            print(" ")
+            if chmil == "1" :
+                print("Seconds-to-milliseconds is " + str(time*1000)+ " milliseconds")
+            elif chmil == "2" :
+                print("Minutes-to-milliseconds is " + str(time*6000)+ " milliseconds")
+            elif chmil == "3" :
+                print("Hours-to-milliseconds is " + str(time*3600000)+ " milliseconds")
+            elif chmil == "4" :
+                print("Days-to-milliseconds is " + str(time*86400000) + " milliseconds")
+            else :
+                print("Error")
+        elif chy == "2" :
+            print("-SubMenu-\n"
+                "!Time-to-seconds!")
+            print("1| Milliseconds-to-seconds\n"
+                "2| Minutes-to-seconds\n"
+                "3| Hours-to-seconds\n"
+                "4| Days-to-seconds")
+            chmil = input("Do your choose(num): ")
+            time = int(input("Input time(int): "))
+            print(" ")
+            if chmil == "1" :
+                print("Millisecond-to-seconds is " + str(float(time/1000))+ " seconds")
+            elif chmil == "2" :
+                print("Minutes-to-seconds is " + str(time*60)+ " seconds")
+            elif chmil == "3" :
+                print("Hours-to-seconds is " + str(time*3600)+ " seconds")
+            elif chmil == "4" :
+                print("Days-to-seconds is " + str(time*86400) + " seconds")
+            else :
+                print("Error")
+        elif chy == "3" :
+            print("-SubMenu-\n"
+                "!Time-to-minutes!")
+            print("1| Milliseconds-to-minutes\n"
+                "2| Seconds-to-minutes\n"
+                "3| Hours-to-minutes\n"
+                "4| Days-to-minutes")
+            chmil = input("Do your choose(num): ")
+            time = int(input("Input time(int): "))
+            print(" ")
+            if chmil == "1" :
+                print("Millisecond-to-minutes is " + str(float(time/60000))+ " minutes")
+            elif chmil == "2" :
+                print("Seconds-to-minutes is " + str(float(time/60))+ " minutes")
+            elif chmil == "3" :
+                print("Hours-to-minutes is " + str(time*60)+ " minutes")
+            elif chmil == "4" :
+                print("Days-to-minutes is " + str(time*1440) + " minutes")
+            else :
+                print("Error")
+        elif chy == "4" :
+            print("-SubMenu-\n"
+                "!Time-to-hours!")
+            print("1| Milliseconds-to-hours\n"
+                "2| Seconds-to-hours\n"
+                "3| Hours-to-hours\n"
+                "4| Days-to-hours")
+            chmil = input("Do your choose(num): ")
+            time = int(input("Input time(int): "))
+            print(" ")
+            if chmil == "1" :
+                print("Millisecond-to-hours is " + str(float(time/36000000))+ " hours")
+            elif chmil == "2" :
+                print("Seconds-to-hours is " + str(float(time/3600))+ " hours")
+            elif chmil == "3" :
+                print("Minutes-to-hours is " + str(float(time/60))+ " hours")
+            elif chmil == "4" :
+                print("Days-to-hours is " + str(time*24) + " hours")
+            else :
+                print("Error")
+        elif chy == "5" :
+            print("-SubMenu-\n"
+                "!Time-to-days!")
+            print("1| Milliseconds-to-days\n"
+                "2| Seconds-to-days\n"
+                "3| Hours-to-days\n"
+                "4| Days-to-days")
+            chmil = input("Do your choose(num): ")
+            time = int(input("Input time(int): "))
+            print(" ")
+            if chmil == "1" :
+                print("Millisecond-to-days is " + str(float(time/86400000))+ " days")
+            elif chmil == "2" :
+                print("Seconds-to-days is " + str(float(time/86400))+ " days")
+            elif chmil == "3" :
+                print("Minutes-to-days is " + str(float(time/1440))+ " days")
+            elif chmil == "4" :
+                print("Hours-to-days is " + str(float(time/24)) + " days")
+            else :
+                print("Error")
+            
     elif ch == "4" :
-        print('Factorial:',eval(str([i for i in range(1,int(input('number -> '))+1)]).replace(', ','*')[1:-1])) #copied from browser
+        print('Factorial:',eval(str([i for i in range(1,int(input('number -> '))+1)]).replace(', ','*')[1:-1]))
     elif ch == "0" :
-        print("|About program| \n"
-            "Version: 1 \n"
-            "Creator: Dimonchikous")
+        print(" |About program| \n"
+            "   Version: 1.1 \n"
+            "   Changes: \n"
+            "   Added: Time-to-time\n"
+            "   Moved: Time-to-milliseconds moved to Time-to-time\n"
+            "   Creator: Dimonchikous")
     else :
         print("Error")
     print(" ")
